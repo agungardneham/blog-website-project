b78a9b21b993cf9d395bd4d6d9c588ca78185888
agungardneham <agungmahendra1305@gmail.com>
2023-06-22 00:56:54 +0700

    finish app.js and all the ejs template

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,6 +12,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 const posts = [];
+const postTitleLower = _.lowerCase(posts.title);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,7 +21,8 @@ app.use(express.static("public"));
 app.get("/", function(req, res){
   res.render("home", {
     homeContent: homeStartingContent,
-    posts: posts
+    posts: posts,
+    postTitleLower: postTitleLower
   });
 })
 
@@ -47,15 +50,17 @@ app.post("/compose", function(req, res){
 })
 
 app.get("/post/:postId", function(req, res){
-  let postName = req.params.postId;
+  let postName = _.lowerCase(req.params.postId);
   posts.forEach(function(a){
-    if(posts.title = postName){
-      console.log("Match success");
-    } else{
-      console.log("Failed");
+    let postTitleLower = _.lowerCase(a.title);
+    if(postTitleLower === postName){
+      res.render("post", {
+        composeTitle: a.title,
+        composePost: a.descPost
+      })
     }
+    })
   })
-})
 
 
 
